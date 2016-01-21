12caa65bcfde30eac4d809228f73f43f3efe7f99
ZerorulzXXX <philipslawycz@gmail.com>
2016-01-21 00:39:01 -0500

    beginning of ending the game
    I can't figure out what is messing up somebody take a look

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
 	public GameObject enemy;
 	public GameObject boss;
@@ -13,29 +15,91 @@ public class GameController : MonoBehaviour {
 	public float startTime;
 	public float waitTime;
 
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver;
+    private bool restart;
+    //private int score;
+
 	// Use this for initialization
 	void Start () {
-		StartCoroutine (spawnWaves ());
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        //score = 0;
+        //UpdateScore();
+        StartCoroutine (spawnWaves ());
 	}
-	//Spawn enemies in waves
-	IEnumerator spawnWaves () {
+	
+    void update ()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.R))
+            {
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+    
+    //Spawn enemies in waves
+	IEnumerator spawnWaves ()
+    {    
 		yield return new WaitForSeconds (startTime);
-		for (int j= 0; j < waves; j++)
-		{
+        while (true)
+        {
+
+            for (int j= 0; j < waves; j++)
+		    {
 			loopCount++;
-			if (loopCount == waves) {
+			if (loopCount == waves)
+            {
 				Vector3 spawnPosition = new Vector3 (0, 8, 0);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (boss, spawnPosition, spawnRotation);
 			}
-			for (int i = 0; i < numEnemies; i++) {
+        
+                for (int i = 0; i < numEnemies; i++)
+            {
 				Vector3 spawnPosition = new Vector3 (Random.Range (-7, 11), Random.Range (5, 8), spawnValues.z);
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (enemy, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds (spawnTime);
 			}
 			yield return new WaitForSeconds (waitTime);
+
+                if (gameOver)
+            {
+                restartText.text = "Press 'R' to Restart";
+                restart = true;
+                break;
+            }
+         }
+           
+
+          
 		}
 	}
+        /*for score
+        public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+        //updates score
+        void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }*/
+        //gameover
+        public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
 }
 	
