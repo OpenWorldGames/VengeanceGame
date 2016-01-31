c13e29024dce42ae7fe9438ed82ba18739d710e7
masoumim <masoumi.mark@gmail.com>
2016-01-30 21:27:56 -0500

    The enemies shoot many lasers
    I still need to fix the fire rate and the spawn location for the shots.

---
+++ Assets/Script/EnemyControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+
+public class EnemyControl : MonoBehaviour {
+
+   
+    
+    // Public vars for guns
+    public GameObject shot;
+    public Transform shotSpawn;
+    
+    
+
+    
+
+
+
+
+
+    private Rigidbody rb;
+    // private int count;
+
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+       // count = 0;
+       // SetCountText();
+       // winText.text = "";
+    }
+
+    void Update()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+    }
+
+  
+}
