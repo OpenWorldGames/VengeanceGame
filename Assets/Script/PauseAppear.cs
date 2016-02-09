08e3da8f8e536d1528a45ee822e3db066d887673
ZerorulzXXX <philipslawycz@gmail.com>
2016-02-09 15:36:03 -0500

    Game pauses
    p now pauses action

---
+++ Assets/Script/PauseAppear.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseAppear : MonoBehaviour 
+{
+
+
+	private bool isShowing;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			if (Time.timeScale == 1)
+			{
+				Time.timeScale = 0;
+			}
+			else
+			{
+				Time.timeScale = 1;
+			}
+		}
+	
+	}
+}
